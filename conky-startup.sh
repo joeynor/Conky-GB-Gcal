5945d2799d6bdc5423a1a0784541f56176b5eb8a
mintfresh <joey.aka.rizal@gmail.com>
2020-01-17 13:08:33 +0800

    Made changes from https://github.com/ConkyPorn/Conky-Grey-Black-1

--- conky-startup.sh
+++ conky-startup.sh
@@ -2,6 +2,8 @@
 
 killall conky
 
-conky -c ~/.conky/conkyrc_Grey-Black1 ;
+conky -b -c ~/.conky/conkyrc_Grey-Black -p 8 &
+#conky -b -c ~/.conky/conkygmail -p 8 &
+#conky -b -c ~/.conky/conkyclock -p 8 &
 
 exit 0
